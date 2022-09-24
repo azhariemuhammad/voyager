c17ec2a4740824efd58da08f85d7314cd86fff88
Azharie Muhammad <azhariemuhammad@MacBook-Air-Azharie.local>
2022-09-24 16:08:52 +0800

    add vite

--- server/renderer.tsx
+++ server/renderer.tsx
@@ -6,30 +6,10 @@ import { StaticRouter } from "react-router-dom/server";
 
 import App from "../src/App";
 
-const clientBundleScript = `<script src="http://localhost:8080/server/bundle.js"></script>`; // [B]
-const clientBundleStyle = `<link rel="stylesheet" href="http://localhost:8080/styles/bundle.css">`; // [B]
-
-function handleRender(req, res) {
-  const reactHtml = ReactDOMServer.renderToString(
-    <StaticRouter location={req.url}>
+export function render(url, context) {
+  return ReactDOMServer.renderToString(
+    <StaticRouter location={url} context={context}>
       <App />
     </StaticRouter>
   );
-  const htmlTemplate = `
-    <!DOCTYPE html>
-      <html>
-          <head>
-              <title>Universal React server bundle</title>
-              ${clientBundleStyle}
-          </head>
-          <body>
-              <div id="app">${reactHtml}</div>
-                ${clientBundleScript}
-          </body>
-    </html>`;
-  res.type("text/html").send(htmlTemplate);
-
-  return res.send(htmlTemplate);
 }
-
-export default handleRender;
--- src/hooks/useGetMediumRSS.ts
+++ src/hooks/useGetMediumRSS.ts
@@ -1,8 +1,8 @@
 import useSWR from "swr";
 import { fetcher } from "../utils/fetcher";
 
-const rssToJSON = process.env.RSS_JSON || "";
-const mediumRss = process.env.MEDIUM_RSS || "";
+const rssToJSON = import.meta.env.VITE_RSS_JSON || "";
+const mediumRss = import.meta.env.VITE_MEDIUM_RSS || "";
 
 const useGetMediumRSS = () => {
   const { data, error } = useSWR(`${rssToJSON}?rss_url=${mediumRss}`, fetcher);
