a0100ca4c818b9afc27b77994659daf228fb5e6f
Azharie Muhammad <azhariemuhammad@MacBook-Air-Azharie.local>
2022-09-29 10:15:53 +0800

    add companies section

---
+++ src/components/Companies.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Companies = () => {
+  const brands = [
+    {
+      name: "gramedia digital nusantara",
+      image:
+        "https://user-images.githubusercontent.com/19600009/192915114-36dd1864-431a-4d86-b276-231862031e2b.png",
+      url: "https://www.gramedia.com/",
+    },
+    {
+      name: "tokopedia",
+      image:
+        "https://user-images.githubusercontent.com/19600009/192915121-822aeafb-e727-42e8-b2e1-82ed76449e8c.png",
+      url: "https://tokopedia.com",
+    },
+    {
+      name: "markoding",
+      url: "https://markoding.org/",
+      image:
+        "https://user-images.githubusercontent.com/19600009/192915883-e6292f69-c6bc-4401-a69a-c3b7fc895ae9.png",
+    },
+  ];
+  return (
+    <div className="mt-20">
+      <h2 className="font-semibold text-gray-600 text-center text">
+        I'm privilage to work with these amazing Companies and Organizations
+      </h2>
+
+      <div className="flex justify-center items-center">
+        {brands.map((brand) => (
+          <a href={brand.url} className="relative">
+            <img className="w-full" src={brand.image} alt={brand.name}></img>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Companies;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AnimateSharedLayout, motion } from "framer-motion";
 import ExperienceList from "../components/Experience";
+import Companies from "../components/Companies";
 
 const container = {
   hidden: { opacity: 1, scale: 0 },
@@ -47,22 +48,17 @@ const About = () => {
         with over 2000 junior devs.
       </p>
       <p className="sm:text-4xl lg:text-base">
-        You can follow me on{" "}
+        I share thoughts on{" "}
         <a href="https://twitter.com/azharieazharou" className="text-amber-500">
           Twitter
         </a>
-        &nbsp;and&nbsp;
-        <a
-          href="https://www.linkedin.com/in/azharie-muhammad/"
-          className="text-amber-500"
-        >
-          LinkedIn
-        </a>
       </p>
+
+      <Companies />
 
       <div className="mt-20 flex flex-col">
         <div>
-          <h2 className="text-xl lg:text-4xl py-6">
+          <h2 className="font-semibold text-gray-600 py-6">
             I've spent most of my development life in Frontend things!
           </h2>
         </div>
@@ -76,7 +72,7 @@ const About = () => {
             <motion.div
               key={idx}
               variants={item}
-              className=" bg-violet-900 w-fit text-xs lg:text-base shadow-violet-400 m-1 text-white w-fit rounded-full cursor-pointer shadow-md hover:shadow-lg  py-2 px-4"
+              className=" bg-violet-900 w-fit text-xs shadow-violet-400 m-1 text-white w-fit rounded-full cursor-pointer shadow-md hover:shadow-lg  py-2 px-4"
             >
               {skill}
             </motion.div>
